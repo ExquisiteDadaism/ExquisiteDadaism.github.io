3ab821e3debee9bc771220c1e6a0ab6ceb69e910
Silverhawk <sondra.silverhawk@hush.com>
2018-09-29 13:47:51 -0700

    Fix animations

--- js/sparrow.js
+++ js/sparrow.js
@@ -7,18 +7,18 @@ function sleep(ms) {
 }
 
 async function rainbows(event) {
-  if($(this).text() == AFTER_TEXT) {
+  if($('#sparrow-text').text() == AFTER_TEXT) {
     return;
   }
   var colors = event.data.colors;
-  $(this).text(AFTER_TEXT);
+  $('#sparrow-text').text(AFTER_TEXT);
   for(var i = 0; i < colors.length; i++) {
     $('#sparrowG').attr('fill', colors[i]);
     await sleep(1000);
     console.log(colors[i]);
   }
   $('#sparrowG').attr('fill', 'black');
-  $(this).text(BEFORE_TEXT);
+  $('#sparrow-text').text(BEFORE_TEXT);
 }
 
 $(document).ready(function() {
@@ -26,6 +26,6 @@ $(document).ready(function() {
     var div = document.getElementById('sparrow');
     div.innerHTML = new XMLSerializer().serializeToString(data.documentElement);
     $('#sparrow').append(`<p id="sparrow-text">${BEFORE_TEXT}</p>`);
-    $('#sparrow-text').click({colors: RAINBOW}, rainbows)
+    $('#sparrow').click({colors: RAINBOW}, rainbows)
   });
 });  
