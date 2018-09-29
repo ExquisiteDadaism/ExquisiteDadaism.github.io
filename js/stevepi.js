4f2c3fdd949d340bdc15934bb38e0752823b7063
Steve Piasecki <stevepi@stevepi.com>
2018-09-29 12:59:03 -0700

    updated animation: now has all 5 images, 5 sec fades

--- js/stevepi.js
+++ js/stevepi.js
@@ -3,30 +3,54 @@
 $(document).ready(function() {
     console.log( "doc.ready complete" );
 function fadeInOut() {
-/*
-  $( ".dadaBG" ).animate( "5", function() {
-      // Animation complete
-      console.log('eye5 fadeIn complete');
-      $(".dadaBG").animate( "5", function() {
-        fadeInOut();
-      });
-    }
-  )
-*/
-
-
-$( ".dadaBG" ).animate({
-    opacity: 1,
-  }, 5000, function() {
-    $( ".dadaBG" ).animate({
-        opacity: 0,
-      }, 5000, function() {
-        fadeInOut();
-  });
-});
-
-
+  $( "#dadaBG" ).removeClass( "dadaBG5" ).addClass( "dadaBG1" );
+  $("#dadaBG").animate({
+      opacity: 1,
+    }, 5000, function() {
+      $("#dadaBG").animate({
+          opacity: 0,
+        }, 5000, function() {
+          $( "#dadaBG" ).removeClass( "dadaBG1" ).addClass( "dadaBG2" );
+          $("#dadaBG").animate({
+              opacity: 1,
+            }, 5000, function() {
+              $("#dadaBG").animate({
+                  opacity: 0,
+                }, 5000, function() {
+                  $( "#dadaBG" ).removeClass( "dadaBG2" ).addClass( "dadaBG3" );
+                  $("#dadaBG").animate({
+                      opacity: 1,
+                    }, 5000, function() {
+                      $("#dadaBG").animate({
+                          opacity: 0,
+                        }, 5000, function() {
+                          $( "#dadaBG" ).removeClass( "dadaBG3" ).addClass( "dadaBG4" );
+                          $("#dadaBG").animate({
+                              opacity: 1,
+                            }, 5000, function() {
+                              $("#dadaBG").animate({
+                                  opacity: 0,
+                                }, 5000, function() {
+                                  $( "#dadaBG" ).removeClass( "dadaBG4" ).addClass( "dadaBG5" );
+                                  $("#dadaBG").animate({
+                                      opacity: 1,
+                                    }, 5000, function() {
+                                      $("#dadaBG").animate({
+                                          opacity: 0,
+                                        }, 5000, function() {
+                                          fadeInOut();
+                                        });
+                                      });
+                                    });
+                                  });
+                                });
+                              });
+                            });
+                          });
+                          });
+                          });
 }
+
 fadeInOut();
 
 
